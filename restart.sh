06223ade63c457c086949b93ffd166ce4bb34e91
Denver De Kock <denver.dekock@itc.co.za>
2025-05-12 11:26:40 +0200

    restart script

--- restart.sh
+++ restart.sh
@@ -1,8 +1,3 @@
-# grafana
-
-docker volume create grafana-storage
-
-
 docker stop grafana-container && docker rm grafana-container && aws sso login && eval "$(aws configure export-credentials --profile jnr-dev --format env)" && docker run -d -p 3000:3000 \
 --name grafana-container \
 -v grafana-storage:/var/lib/grafana \
@@ -10,5 +5,3 @@ docker stop grafana-container && docker rm grafana-container && aws sso login &&
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
   grafana/grafana
-
-alias restart='/home/denverdk/workspace/personal/grafana/restart.sh'
